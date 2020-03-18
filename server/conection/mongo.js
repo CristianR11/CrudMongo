831ef8b09f6a3d653dc2eb405369598aa2167347
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-03-18 16:10:39 -0500

    UpdateCors

--- server/conection/mongo.js
+++ server/conection/mongo.js
@@ -18,6 +18,12 @@ module.exports = function(app){
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
 
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	  });
+
 	const options = {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
